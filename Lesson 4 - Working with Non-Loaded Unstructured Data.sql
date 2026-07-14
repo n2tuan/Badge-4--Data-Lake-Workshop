0e7c5b4e3a3f116a3473b42e744789fa9820ce04
TuanNN <tuannn@yahoo.com>
2026-07-14 15:29:11 +0700

    feat: Complete lesson 4

--- Lesson 4 - Working with Non-Loaded Unstructured Data.sql
+++ Lesson 4 - Working with Non-Loaded Unstructured Data.sql
@@ -55,3 +55,56 @@ select GRADER(step, (actual = expected), actual, expected, description) as grade
  ,180 as expected
  ,'Cross-joined view exists' as description
 ); 
+-- Add a table to map the sweatsuits to the sweat band sets
+create table zenas_athleisure_db.products.upsell_mapping
+(
+sweatsuit_color_or_style varchar(25)
+,upsell_product_code varchar(10)
+);
+
+--populate the upsell table
+insert into zenas_athleisure_db.products.upsell_mapping
+(
+sweatsuit_color_or_style
+,upsell_product_code 
+)
+VALUES
+('Charcoal Grey','SWT_GRY')
+,('Forest Green','SWT_FGN')
+,('Orange','SWT_ORG')
+,('Pink', 'SWT_PNK')
+,('Red','SWT_RED')
+,('Yellow', 'SWT_YLW');
+-- Zena needs a single view she can query for her website prototype
+create view catalog_for_website as 
+select color_or_style
+,price
+,file_name
+, get_presigned_url(@sweatsuits, file_name, 3600) as file_url
+,size_list
+,coalesce('Consider: ' ||  headband_description || ' & ' || wristband_description, 'Consider: White, Black or Grey Sweat Accessories')  as upsell_product_desc
+from
+(   select color_or_style, price, file_name
+    ,listagg(sizes_available, ' | ') within group (order by sizes_available) as size_list
+    from catalog
+    group by color_or_style, price, file_name
+) c
+left join upsell_mapping u
+on u.sweatsuit_color_or_style = c.color_or_style
+left join sweatband_coordination sc
+on sc.product_code = u.upsell_product_code
+left join sweatband_product_line spl
+on spl.product_code = sc.product_code;
+use role accountadmin;
+use schema util_db.public;
+select GRADER(step, (actual = expected), actual, expected, description) as graded_results from
+(
+SELECT
+'DLKW04' as step
+ ,( select count(*) 
+  from zenas_athleisure_db.products.catalog_for_website 
+  where upsell_product_desc not like '%e, Bl%') as actual
+ ,6 as expected
+ ,'Relentlessly resourceful' as description
+);
+select * from zenas_athleisure_db.products.catalog_for_website limit 5;
