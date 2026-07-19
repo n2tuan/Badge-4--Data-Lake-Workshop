5877a5a480c3ad8c99896e9286652c1deb9a780b
TuanNN <tuannn@yahoo.com>
2026-07-19 13:37:35 +0700

    Complete lesson 7

---
+++ Lesson 7 - Exploring Geospatial Functions.sql
@@ -0,0 +1,82 @@
+--Remember this code? 
+use role sysadmin;
+use schema MELS_SMOOTHIE_CHALLENGE_DB.TRAILS;
+select 
+'LINESTRING('||
+listagg(coord_pair, ',') 
+within group (order by point_id)
+||')' as my_linestring
+,st_length(to_geography(my_linestring)) as length_of_trail --this line is new! but it won't work!
+from cherry_creek_trail
+group by trail_name;
+
+select top 5 *,
+st_length(TO_GEOGRAPHY(GEOMETRY)) as trailength
+from denver_area_trails;
+create or replace view DENVER_AREA_TRAILS as 
+select
+$1:features[0]:properties:Name::string as feature_name
+,$1:features[0]:geometry:coordinates::string as feature_coordinates
+,$1:features[0]:geometry::string as geometry
+,st_length(TO_GEOGRAPHY(GEOMETRY)) as trail_length
+,$1:features[0]:properties::string as feature_properties
+,$1:crs:properties:name::string as specs
+,$1 as whole_object
+from @trails_geojson (file_format => ff_json);
+
+--Create a view that will have similar columns to DENVER_AREA_TRAILS 
+--Even though this data started out as Parquet, and we're joining it with geoJSON data
+--So let's make it look like geoJSON instead.
+create or replace view DENVER_AREA_TRAILS_2 as
+select 
+trail_name as feature_name
+,'{"coordinates":['||listagg('['||lng||','||lat||']',',') within group (order by point_id)||'],"type":"LineString"}' as geometry
+,st_length(to_geography(geometry))  as trail_length
+from cherry_creek_trail
+group by trail_name;
+--Create a view that will have similar columns to DENVER_AREA_TRAILS 
+select feature_name, geometry, trail_length
+from DENVER_AREA_TRAILS
+union all
+select feature_name, geometry, trail_length
+from DENVER_AREA_TRAILS_2;
+
+create or replace view trails_and_boundaries as
+--Add more GeoSpatial Calculations to get more GeoSpecial Information! 
+select feature_name
+, to_geography(geometry) as my_linestring
+, st_xmin(my_linestring) as min_eastwest
+, st_xmax(my_linestring) as max_eastwest
+, st_ymin(my_linestring) as min_northsouth
+, st_ymax(my_linestring) as max_northsouth
+, trail_length
+from DENVER_AREA_TRAILS
+union all
+select feature_name
+, to_geography(geometry) as my_linestring
+, st_xmin(my_linestring) as min_eastwest
+, st_xmax(my_linestring) as max_eastwest
+, st_ymin(my_linestring) as min_northsouth
+, st_ymax(my_linestring) as max_northsouth
+, trail_length
+from DENVER_AREA_TRAILS_2;
+select 'POLYGON(('|| 
+    min(min_eastwest)||' '||max(max_northsouth)||','|| 
+    max(max_eastwest)||' '||max(max_northsouth)||','|| 
+    max(max_eastwest)||' '||min(min_northsouth)||','|| 
+    min(min_eastwest)||' '||min(min_northsouth)||'))' AS my_polygon
+from trails_and_boundaries;
+
+use role ACCOUNTADMIN;
+use schema util_db.public;
+
+select GRADER(step, (actual = expected), actual, expected, description) as graded_results from
+(
+ SELECT
+  'DLKW07' as step
+   ,( select round(max(max_northsouth))
+      from MELS_SMOOTHIE_CHALLENGE_DB.TRAILS.TRAILS_AND_BOUNDARIES)
+      as actual
+ ,40 as expected
+ ,'Trails Northern Extent' as description
+ ); 
